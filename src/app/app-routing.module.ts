89de832a0cb6ed37f827dc123d8f843f257e4e47
Becario Morales <luis_muscul30@hotmail.com>
2023-01-16 14:06:37 -0600

    inicio->Index y component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Index1Component } from './index1/index1.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'get/:variable',
+  component:Index1Component
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
